de0f0a3db7faa1e840d13e048490e100751e1775
q-13t <vova29092002@gmail.com>
2025-05-18 15:28:08 +0300

    WIP: WebSocket

---
+++ src/main/java/edu/chat/controllers/WEBSocketController.java
@@ -0,0 +1,189 @@
+package edu.chat.controllers;
+
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.java_websocket.WebSocket;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import edu.chat.routes.UserRoutes;
+import edu.chat.views.User;
+
+@Component
+public class WEBSocketController extends WebSocketServer {
+    private static WEBSocketController WEBSC;
+    private static HashMap<String, WebSocket> clients = new HashMap<>();
+    Logger log = Logger.getLogger(WEBSocketController.class.getName());
+
+    private static enum RequestType {
+        getMessage, sendMessage, deleteMessage, updateMessage, getChats, sendFile, deleteFile, updateFile, getUsersByName,
+    }
+
+    @Autowired
+    private UserRoutes userService;
+
+    public static String getServerData() {
+        return WEBSC.getAddress().getAddress().getHostAddress() + ":" + WEBSC.getPort();
+    }
+
+    public static String parseUserShort(WebSocket conn) {
+        return conn.getRemoteSocketAddress().getAddress().getHostAddress() + ":" + conn.getRemoteSocketAddress().getPort();
+    }
+
+    public WEBSocketController() {
+    }
+
+    @Bean
+    public WEBSocketController initController() {
+        WEBSC = new WEBSocketController(new InetSocketAddress(8081));
+        WEBSC.setConnectionLostTimeout(60_000);
+        WEBSC.start();
+        Thread connectionStatusThread = new Thread() {
+            @Override
+            public void run() {
+                StringBuilder SB = new StringBuilder();
+                while (true) {
+                    try {
+                        Thread.sleep(60_000);
+                    } catch (InterruptedException e) {
+                    }
+                    SB.replace(0, SB.length(), "");
+                    Collection<WebSocket> connections = WEBSC.getConnections();
+                    SB.append("Server Connections: [" + connections.size() + "]");
+                    // for (WebSocket webSocketWorker : connections) {
+                    // SB.append("\n"+webSocketWorker.getRemoteSocketAddress() );
+                    // }
+                    // log.info(SB.toString());
+                }
+            }
+        };
+        connectionStatusThread.setDaemon(true);
+        connectionStatusThread.start();
+        return WEBSC;
+    }
+
+    public WEBSocketController(int port) throws UnknownHostException {
+        super(new InetSocketAddress(port));
+    }
+
+    public WEBSocketController(InetSocketAddress address) {
+        super(address);
+    }
+
+    public WEBSocketController(int port, Draft_6455 draft) {
+        super(new InetSocketAddress(port), Collections.<Draft>singletonList(draft));
+    }
+
+    private static JsonObject prepareErrorResponse(String message) {
+        JsonObject response = new JsonObject();
+        response.addProperty("type", "error");
+        response.addProperty("message", message);
+        return response;
+    }
+
+    private JsonObject prepareGetUsersByUsernameResponse(RequestType requestType, JsonObject request) {
+        JsonObject response = new JsonObject();
+        String searchUsername = request.get("username").getAsString();
+        String username = userService.getUsernameByToken(request.get("token").getAsString());
+        JsonObject data = request.getAsJsonObject("data");
+        if (username == null) {
+            log.error("Invalid token: " + data.get("token").getAsString());
+            response = prepareErrorResponse("Invalid token: " + data.get("token").getAsString());
+            return response;
+        }
+        if (searchUsername == null) {
+            log.error("Invalid username: " + searchUsername);
+            response = prepareErrorResponse("Invalid username: " + searchUsername);
+            return response;
+        }
+
+        List<User> usersByUsername = userService.getUsersByUsername(searchUsername);
+        if (usersByUsername == null || usersByUsername.isEmpty()) {
+            log.error("No users found with username: " + searchUsername);
+            response = prepareErrorResponse("No users found with username: " + searchUsername);
+            return response;
+        }
+
+        JsonArray list = new JsonArray();
+        for (User user : usersByUsername) {
+            JsonObject userJson = new JsonObject();
+            userJson.addProperty("id", user.getId());
+            userJson.addProperty("username", user.getUsername());
+            list.add(userJson);
+        }
+        response.addProperty("type", requestType.toString());
+        response.addProperty("users", list.toString());
+        return response;
+
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        log.info("New connection: " + parseUserShort(conn));
+        clients.put(conn.getRemoteSocketAddress().getAddress().getHostAddress() + ":" + conn.getRemoteSocketAddress().getPort(), conn);
+        log.info("Total connections: " + clients.size());
+        log.info("Handshake: " + handshake.getResourceDescriptor() + " " + handshake.getFieldValue("Sec-WebSocket-Key") + " " + handshake.getFieldValue("Sec-WebSocket-Protocol"));
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        log.info("Closed connection: " + parseUserShort(conn));
+        clients.remove(parseUserShort(conn));
+        log.info("Total connections: " + clients.size());
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        log.info("Message from " + parseUserShort(conn) + ": " + message);
+        JsonObject request = new Gson().fromJson(message, JsonObject.class);
+        try {
+            userService.validateToken(request.get("token").getAsString());
+        } catch (Exception e) {
+            log.error("Invalid token: " + e.getMessage());
+            conn.close(1000, "Invalid token: " + e.getMessage());
+            return;
+        }
+        ;
+        // Here all the checks passed
+        JsonObject response = new JsonObject();
+        RequestType requestType = RequestType.valueOf(request.get("type").getAsString());
+        switch (requestType) {
+        case getUsersByName: {
+            response = prepareGetUsersByUsernameResponse(requestType, request);
+            break;
+        }
+
+        default: {
+            response = prepareErrorResponse("Invalid request type: " + requestType.toString());
+            log.error("Invalid request type: " + requestType.toString());
+            break;
+        }
+        }
+        conn.send(response.toString());
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        log.error(ex.getMessage());
+    }
+
+    @Override
+    public void onStart() {
+        log.info("ChatServer started on: " + getServerData());
+    }
+
+}
