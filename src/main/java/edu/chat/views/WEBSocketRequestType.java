3b1d1acbc0dad9cc7091fc287a6524bcef9f9e44
q-13t <vova29092002@gmail.com>
2025-05-28 15:48:17 +0300

    UPD: Enums

--- src/main/java/edu/chat/views/WEBSocketRequestType.java
+++
@@ -1,5 +0,0 @@
-package edu.chat.views;
-
-public enum WEBSocketRequestType {
-    getMessage, sendMessage, deleteMessage, updateMessage, getChats, sendFile, deleteFile, updateFile, getUsersByName, getPFP
-}
---
+++ src/main/java/edu/chat/views/enums/WEBSocketRequestType.java
@@ -0,0 +1,5 @@
+package edu.chat.views.enums;
+
+public enum WEBSocketRequestType {
+    getMessage, sendMessage, deleteMessage, updateMessage, getChats, sendFile, deleteFile, updateFile, getUsersByName, getPFP, newChatPrivate
+}
